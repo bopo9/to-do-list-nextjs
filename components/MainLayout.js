ba384620e3f85046a3c62f0a698ecac7518f2278
Boris Shcherbina <moodmusicm@mail.com>
2021-12-19 18:43:07 +0200

    added styles for mainLayout and added page todos

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -4,16 +4,21 @@ import styles from "../styles/main.module.sass"
 export default function mainLayout({ children }){
     return (
         <div className={styles.main_wrapper}>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href={'/'}>
-                            <a>Home</a>
+            <nav className={styles.navbar}>
+                <ul className={styles.navbar_nav}>
+                    <li className={styles.navbar_item}>
+                        <Link  href={'/'}>
+                            <a className={styles.navbar_link}>Home</a>
                         </Link>
                     </li>
-                    <li>
+                    <li className={styles.navbar_item}>
                         <Link href={'/todos'}>
-                            <a>Todos</a>
+                            <a className={styles.navbar_link}>Todos</a>
+                        </Link>
+                    </li>
+                    <li className={styles.navbar_item}>
+                        <Link href={'/completed'}>
+                            <a className={styles.navbar_link}>Completed Task's</a>
                         </Link>
                     </li>
                 </ul>
---
+++ pages/todos/index.js
@@ -0,0 +1,9 @@
+import MainComponent from '../../components/MainLayout'
+
+export default function todos(){
+    return (
+        <MainComponent>
+            <h1>todos</h1>
+        </MainComponent>
+    )
+}
