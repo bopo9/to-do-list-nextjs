855b58c305758170a0364868358ea26ac61960a4
Boris Shcherbina <moodmusicm@mail.com>
2021-12-19 20:19:12 +0200

    added styles for page todos

--- pages/todos/index.js
+++ pages/todos/index.js
@@ -1,9 +1,15 @@
 import MainComponent from '../../components/MainLayout'
+import classes from "../../styles/todos.module.sass";
 
 export default function todos(){
     return (
         <MainComponent>
-            <h1>todos</h1>
+            <div className={classes.wrapper}>
+                <div className={classes.todo_item}>
+                    <p className={classes.todo_text}>Сделать какое-то действие блабла </p>
+                    <button className={classes.btn_close}>Delete</button>
+                </div>
+            </div>
         </MainComponent>
     )
 }
